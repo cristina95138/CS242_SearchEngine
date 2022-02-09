a721363f5d4dec4783a1d66b78df406cb998d117
Cristina Lawson <30380172+cristina95138@users.noreply.github.com>
2022-02-09 01:45:17 -0800

    webscraper

--- webscraper/jobsearch/jobsearch/settings.py
+++ webscraper/jobsearch/jobsearch/settings.py
@@ -12,6 +12,7 @@ BOT_NAME = 'jobsearch'
 SPIDER_MODULES = ['jobsearch.spiders']
 NEWSPIDER_MODULE = 'jobsearch.spiders'
 
+LOG_ENABLED = False
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'jobsearch (+http://www.yourdomain.com)'
@@ -20,14 +21,14 @@ NEWSPIDER_MODULE = 'jobsearch.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
