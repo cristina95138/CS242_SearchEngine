4375576925d11100ed03a15b662e147f62b6703d
Cristina Lawson <30380172+cristina95138@users.noreply.github.com>
2022-02-09 02:38:46 -0800

    webscraper

--- webscraper/jobsearch/jobsearch/settings.py
+++ webscraper/jobsearch/jobsearch/settings.py
@@ -28,7 +28,7 @@ CONCURRENT_REQUESTS = 32
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
